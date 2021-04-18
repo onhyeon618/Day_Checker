97b9430a997a64fa31c904364c9a5eef1a2c696b
Hyeon_ <onhyeon618@gmail.com>
2021-04-18 18:45:35 +0900

    Create xml files, edit login page xml

--- DayChecker/app/src/main/java/com/toyprojects/daychecker/MainActivity.kt
+++ DayChecker/app/src/main/java/com/toyprojects/daychecker/MainActivity.kt
@@ -2,10 +2,16 @@ package com.toyprojects.daychecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.toyprojects.daychecker.R
+import com.toyprojects.daychecker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
