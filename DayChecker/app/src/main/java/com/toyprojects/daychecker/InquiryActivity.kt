9c0b3947c7f8edb719d3e52639b3388610e39ff9
Hyeon_ <onhyeon618@gmail.com>
2021-04-24 23:50:41 +0900

    Complete inquiry page layout + change colorPrimary
    * colorPrimary changed to main theme color; set toolbar background color explicitly

--- DayChecker/app/src/main/java/com/toyprojects/daychecker/InquiryActivity.kt
+++ DayChecker/app/src/main/java/com/toyprojects/daychecker/InquiryActivity.kt
@@ -1,6 +1,7 @@
 package com.toyprojects.daychecker
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.toyprojects.daychecker.databinding.ActivityInquiryBinding
 
@@ -10,13 +11,19 @@ class InquiryActivity : AppCompatActivity() {
         val binding = ActivityInquiryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var topAppBar = binding.toolbar
+        val topAppBar = binding.toolbar
         topAppBar.setNavigationIcon(R.drawable.back_button);
+        topAppBar.setBackgroundColor(255)
         topAppBar.setTitle(R.string.inquiries)
 
         topAppBar.setNavigationOnClickListener {
             // 이후 작성 내용 있으면 Alert 띄우도록 수정 필요
              finish()
         }
+
+        // Set texts for inquiry-type spinner
+        val typeList = listOf("문의종류1", "문의종류2", "문의종류3", "기타")
+        val spnAdapter = ArrayAdapter<String>(this, R.layout.dropdown_item, typeList)
+        binding.spnInquiryType.setAdapter(spnAdapter)
     }
 }
